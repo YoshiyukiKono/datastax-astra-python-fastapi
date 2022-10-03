1e261f92a6d8db14e766f12647ac5a5e4a788ba8
Yoshiyuki Kono <yoshiyuki.kono@gmail.com>
2022-10-03 15:52:16 +0900

    Update and rename connect_astra.py to test/connect_astra.py

--- test/connect_astra.py
+++ test/connect_astra.py
@@ -2,9 +2,9 @@ from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 
 cloud_config= {
-        'secure_connect_bundle': './exclude/secure-connect-netlify.zip'
+        'secure_connect_bundle': './exclude/secure-connect-<<YOUR DB NAME>>.zip'
 }
-auth_provider = PlainTextAuthProvider('WGXMvkZitjTUJFlkDZKEjhph', 'bOSCbHGLu-6HQg5_KEluRCYMD_E.-STR1dMin,EE00xrsHoysy-MaHxPj5uRu33qRZW-v3idJbWA06osSJNNLS6yt+w1,K-lB-tuFv78YK9lmsrKGFW+WECM2UFZ48_A')
+auth_provider = PlainTextAuthProvider('<<CLIENT ID>>', '<<CLIENT SECRET>>')
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
 session = cluster.connect()
 
